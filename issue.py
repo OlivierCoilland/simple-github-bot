a55322de84d9e463fc44729aa5d1f3a843df9eb1
Olivier Coilland <olivier@coilland.fr>
2017-04-21 17:15:52 +0200

    Can add label to issue.

--- issue.py
+++ issue.py
@@ -5,9 +5,17 @@ class Issue(GitHubObject):
     def __init__(self, session, data):
         super().__init__(session, data)
         self.COMMENTS_URL = data['comments_url']
+        self.LABELS_URL = data['labels_url'].replace('{/name}', '')
+        self.LABEL_URL = data['labels_url'].replace('{/name}', '/{}')
 
     def add_comment(self, body):
         url = self.COMMENTS_URL
         data = {'body': body}
+        r = self.session.post(url, json=data)
+        r.raise_for_status()
+
+    def add_label(self, label):
+        url = self.LABELS_URL
+        data = [ label ]
         r = self.session.post(url, json=data)
         r.raise_for_status()
--- test.py
+++ test.py
@@ -5,4 +5,5 @@ github = GitHub(config.token)
 repository = github.get_repo(config.repository)
 issue = repository.get_issue(1);
 
-issue.add_comment("Hey! I'm a bot :)")
+# issue.add_comment("Hey! I'm a bot :)")
+issue.add_label('bug')
