77b9d4411b369ac4027935980ea8e0611ab47316
Olivier Coilland <olivier@coilland.fr>
2017-04-22 21:37:10 +0200

    Get comments and events for issue.

--- issue.py
+++ issue.py
@@ -6,8 +6,19 @@ class Issue(GitHubObject):
         super(Issue, self).__init__(session, data)
         self.issue_url = data['url']
         self.comments_url = data['comments_url']
+        self.events_url = data['events_url']
         self.labels_url = data['labels_url'].replace('{/name}', '')
         self.label_url = data['labels_url'].replace('{/name}', '/{}')
+
+    def get_comments(self):
+        r = self.session.get(self.comments_url)
+        r.raise_for_status()
+        return r.json()
+
+    def get_events(self):
+        r = self.session.get(self.events_url)
+        r.raise_for_status()
+        return r.json()
 
     def add_comment(self, body):
         data = {'body': body}
