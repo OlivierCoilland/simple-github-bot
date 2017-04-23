fae731fcc2953aeaf45d3df969dd0f9ac8f1d148
Olivier Coilland <olivier@coilland.fr>
2017-04-23 16:09:11 +0200

    Working POC of bot.

--- config-example.py
+++ config-example.py
@@ -1,4 +1,13 @@
 token = 'CHANGE_ME'
+
 repository = 'USER/REPO'
+
 days_before_warning = 30
+
 days_between_warning_and_closing = 15
+
+warning_message = ("Hi @{login}, this issue looks inactive and I'm about to close it.\n"
+"#ignore #warning")
+
+closing_message = ("This issue has been confirmed inactive, I'm closing it.\n"
+"#ignore")
--- test.py
+++ test.py
@@ -3,20 +3,28 @@ from github import GitHub
 import config
 import strategy
 
+def warn(issue):
+    text = format_message(issue, config.warning_message)
+    issue.add_comment(text)
+
+def close(issue):
+    text = format_message(issue, config.closing_message)
+    issue.add_comment(text)
+    issue.close()
+
+def format_message(issue, text):
+    issue_author_login = issue.data['user']['login']
+    return text.format(login=issue_author_login)
+
 github = GitHub(config.token)
 repository = github.get_repo(config.repository)
 
 strategy = strategy.LastCommentStrategy()
 issues = repository.get_issues()
 for issue in issues:
-    print(strategy.should_warn(issue))
-    print(strategy.should_close(issue))
-
-# def warn_before_closing(issue):
-#     text = "Hi {login}!\nThis issue looks inactive\nI'm about to close it!"
-#     issue_author_login = issue.data['user']['login']
-#     text = text.format(login=issue_author_login)
-#     issue.add_comment(text)
-
-# warn_before_closing(issue)
-# issue.close()
+    if strategy.should_close(issue):
+        close(issue)
+        continue
+    if strategy.should_warn(issue):
+        warn(issue)
+        continue
